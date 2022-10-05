a07c29f521010a88bdc94720165048bb9b9aeb79
Aashutosh <aashutosh@asleepbynine.dev>
2022-10-05 08:00:26 +0545

    enable spell check and wrapping for markdown files

---
+++ dotfiles/nvim/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_option_value("wrap", true, { scope = "local" })
+vim.api.nvim_set_option_value("linebreak", true, { scope = "local" })
+vim.api.nvim_set_option_value("spell", true, { scope = "local" })
